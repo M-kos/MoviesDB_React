8f323462252e24f89675b081dffb7c7e1a4b6eb5
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-03 19:30:27 +0300

    Пропсы, валидация, вставил по местам

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Rate } from 'antd';
+import { format } from 'date-fns';
 
 import truncateText from '../../utils/truncateText';
 
 import './MovieCard.css';
 
-const MovieCard = () => {
+const MovieCard = ({
+  movie: { title, overview: description, release_date: releaseDate, vote_average: titleRating },
+  imageUrl,
+}) => {
   return (
     <div className="movieCard">
-      <img className="movieCard__image" src="https://via.placeholder.com/183x281" alt="poster" />
+      <img className="movieCard__image" src={imageUrl} alt="poster" />
       <div className="movieCard__summary">
         <div className="movieCard__title">
-          <h2 className="movieCard__title-inscription">The way back</h2>
-          <div className="movieCard__title-rating">6.6</div>
+          <h2 className="movieCard__title-inscription">{title}</h2>
+          <div className="movieCard__title-rating">{titleRating}</div>
         </div>
-        <div className="movieCard__date">March 5, 2020</div>
+        <div className="movieCard__date">{format(new Date(releaseDate), 'MMMM d, yyyy')}</div>
         <div className="movieCard__genres">Action</div>
-        <div className="movieCard__description">
-          {truncateText(
-            'A former basketball all-star, who has lost his wife and family foundation in a struggle with addiction attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high addiction attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high addiction attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high addiction attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high'
-          )}
-        </div>
+        <div className="movieCard__description">{truncateText(description)}</div>
         <div className="movieCard__fill" />
         <Rate disabled="true" allowClear="false" allowHalf defaultValue={2.5} />
       </div>
     </div>
   );
+};
+
+MovieCard.defaultProps = {
+  movie: {
+    title: '',
+    overview: '',
+    release_date: '',
+    vote_average: 0,
+  },
+  imageUrl: '',
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    overview: PropTypes.string,
+    release_date: PropTypes.string,
+    vote_average: PropTypes.number,
+  }),
+  imageUrl: PropTypes.string,
 };
 
 export default MovieCard;
