b159a8046d06dba11db4db164d141a9a6a24ef03
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-03 19:30:10 +0300

    Запрос и отрисовка карточек

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,31 +7,43 @@ import MovieDbService from '../../services/movieDbService';
 import 'antd/dist/antd.css';
 import './App.css';
 
+const movieDbService = new MovieDbService();
 export default class App extends Component {
-  async componentDidMount() {
-    const movieDbService = new MovieDbService();
+  state = {};
 
+  async componentDidMount() {
     const res = await movieDbService.getMovies('return');
 
-    console.log('res', res);
+    if (res) {
+      this.setState(res);
+    }
   }
 
   render() {
+    const { results } = this.state;
+
+    let cards = null;
+
+    if (results) {
+      cards = results.slice(0, 6).map((movie) => {
+        return (
+          <Col key={movie.id} md={12} sm={24}>
+            <MovieCard movie={movie} imageUrl={movieDbService.getImage(movie.poster_path)} />
+          </Col>
+        );
+      });
+    }
+
     return (
       <div className="app">
         <Row
           gutter={[
-            { xs: 16, sm: 20, md: 36 },
+            { xs: 16, sm: 16, md: 36 },
             { xs: 20, sm: 20, md: 36 },
           ]}
           sm={24}
         >
-          <Col md={12} sm={24}>
-            <MovieCard />
-          </Col>
-          <Col md={12} sm={24}>
-            <MovieCard />
-          </Col>
+          {cards}
         </Row>
       </div>
     );
